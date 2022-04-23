4c24be8bccc4eebfb2d41719bfb3995e689d01f6
romanprotoliuk <romanprotoliuk@gmail.com>
2022-04-22 23:14:09 -0700

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     axios.get(`https://api.hatchways.io/assessment/students`)
       .then(res => {
         const students = res.data.students
-        console.log(students)
         setStudentsData(students)
       })
   }
@@ -19,23 +18,24 @@ const App = () => {
 
     const helperFuncAverage = (array) => {
       const sum = array.reduce((prev, cur) => parseInt(prev) + parseInt(cur), 0)
-      const arrLength = array.length
-      const average = sum / arrLength
+      const average = sum / array.length
       
       return average
     }
     
   const eachStudent = studentsData.map((student, i) => {
-    return <div className='wrapper-student' key={i}><img src={student.pic} alt={`picture for ${student.firstName} ${student.lastName}`} /> <div className='fullname-inline'><h2 className='firstName'>{student.firstName}</h2><h2 className='lastName'>{student.lastName}</h2></div><p>Email: {student.email}</p><p>Company: {student.company}</p><p>Skill: {student.skill}</p><p>average: {helperFuncAverage(student.grades)}%</p></div> 
+    return <div className='outter-wrapper'><div className = 'wrapper-student' key = { i } ><img className='img' src={student.pic} alt={`picture for ${student.firstName} ${student.lastName}`} /><div><div className='fullname-inline'><h2 className='firstName'>{student.firstName}</h2><h2 className='lastName'>{student.lastName}</h2></div><div className='wrapper-inner-content'><p className='content'>Email: {student.email}</p><p className='content'>Company: {student.company}</p><p className='content'>Skill: {student.skill}</p><p className='content'>average: {helperFuncAverage(student.grades)}%</p></div></div></div></div>
   })
   
   
 
 
   return (
-    <>
-      {eachStudent}
-    </>
+    <div className='most-outter-wrapper'>
+      <div className='wrapper-holder'>
+        {eachStudent}
+      </div>
+    </div>
   );
 }
 
